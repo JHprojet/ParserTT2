665eb998fa41b02d6c01f02c4c7cf7cf5f7d5d10
JHprojet <61949123+JHprojet@users.noreply.github.com>
2021-09-29 17:07:54 +0200

    Remove console log type

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function JSONobjectToCsv(AllRaids)
 }
 
 function add(A){
-    console.log(A + ":" + typeof(A));
+    //console.log(A + ":" + typeof(A));
     if(typeof(A)=="string") return ";'"+A;
     if(typeof(A)=="number") return ";"+Math.round(A);
 }
